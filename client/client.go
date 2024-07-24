5d2f24167094b2b573e2cde50fd0287ce1b2f1a7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-24 12:41:56 +0600

    PostFormData

--- client/client.go
+++ client/client.go
@@ -1,25 +1,37 @@
 package client
 
 import (
-	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 func GetRequest(url string) ([]byte, error) {
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println(err)
 		return nil, err
 	}
 
 	resBody, readError := io.ReadAll(response.Body)
 
 	if readError != nil {
-		fmt.Println(readError)
 		return nil, readError
 	}
 
 	return resBody, nil
 
+}
+
+func PostFormData(url string, data url.Values) ([]byte, error) {
+	response, error := http.PostForm(url, data)
+	if error != nil {
+		return nil, error
+	}
+	responseData, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return responseData, nil
 }
--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/OlyMahmudMugdho/go-http-client/client"
 )
 
 func main() {
-	url := "http://localhost:8080"
+	myUrl := "http://localhost:8080"
 
-	data, _ := client.GetRequest(url)
-
+	data, _ := client.GetRequest(myUrl)
 	fmt.Println(string(data))
+
+	form := url.Values{}
+	form.Add("name", "mugdho")
+	formUrl := myUrl + "/post"
+	formData, _ := client.PostFormData(formUrl, form)
+	fmt.Println(string(formData))
 }
