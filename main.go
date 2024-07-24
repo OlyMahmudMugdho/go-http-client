f29623afd84b355053b53a071933fb8b62bb5b4f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-24 12:17:44 +0600

    updated code

--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
+
+	"github.com/OlyMahmudMugdho/go-http-client/client"
 )
 
 func main() {
 	url := "http://localhost:8080"
 
-	data, _ := GetRequest(url)
+	data, _ := client.GetRequest(url)
 
 	fmt.Println(string(data))
-}
-
-func GetRequest(url string) ([]byte, error) {
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	resBody, readError := io.ReadAll(response.Body)
-
-	if readError != nil {
-		fmt.Println(readError)
-		return nil, readError
-	}
-
-	return resBody, nil
-
 }
